928868e34eb30ea3e377bb000b5d1d679f5036dc
askaik2007-crypto <a.skaik2007@gmail.com>
2026-09-20 18:44:14 +0300

    Force HTTPS for assets and routes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use App\Models\Student;
 use App\Observers\StudentObserver;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,7 +23,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Student::observe(StudentObserver::class);
-
+        if (config('app.env') === 'production' || request()->header('X-Forwarded-Proto') === 'https') {
+        URL::forceScheme('https');
+    }
     }
 
 
